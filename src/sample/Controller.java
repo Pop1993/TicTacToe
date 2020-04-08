a36ee891af9335c55878a406ab5dc9c24942de11
Pop1993 <cristian.pop1993@gmail.com>
2020-04-08 21:35:44 +0300

    Found a way to set the text to 'X' on one clicked button. Commiting the design and switching to a different branch for the functionalities of the game.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,18 @@
 package sample;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+
+
+import java.io.IOException;
 
 public class Controller {
+
+    @FXML
+    private Button button1;
+
+    public void buttonIsClicked(ActionEvent event) throws IOException {
+        button1.setText("X");
+    }
 }
